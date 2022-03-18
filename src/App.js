37bc5f602ad89386def4edaf5818e6448e326761
luiz-cazarin <57879201+luiz-cazarin@users.noreply.github.com>
2022-03-18 00:46:51 -0300

    default app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <p className="header-title">Luiz Claudio</p>
+        <ul className="sub-header">
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <Home/>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import '../App.css';
+function Home () {
+    return (
+        <div className="card">
+            <h3>Home page</h3>
+        </div>
+    )
+}
+
+
+export default Home
